46307bd5559cdaf38e4774e19526337172b1e90a
Salvatore Aresco <57394934+arscslvt@users.noreply.github.com>
2021-02-01 01:16:40 +0100

    bug fixes and adjustments

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ home.addEventListener("click", function(){
     document.location.assign("index.html");
 })
 
-newNoti();
-
 const infoTitle = document.getElementById("infoTitle");
 const infoButton = document.getElementById("goTo");
 
-infoTitle.innerHTML = "Hey, qui non avrai bisogno di Flash Player!";
+infoTitle.innerHTML = "Hey, qui non avrai bisogno di <u>Flash Player</u>!";
+infoButton.innerHTML = "Usa il calibro";
 
 const nav1 = document.getElementById("nav1");
 const nav2 = document.getElementById("nav2");
@@ -21,3 +20,9 @@ function navpage1(){
     document.body.style.animation = "fadeOut 0.5s ease";
     setTimeout(function(){document.location.assign("menu.html")}, 300);
 }
+
+const credits = document.getElementById("credits");
+
+credits.addEventListener("click", function(){
+    newNoti("Crediti", "<b>Programmatore e designer:</b> Aresco Salvatore <br><b>Editor:</b> Gabriele Foti<br><br>Tutte i contenuti e le immagini sono state realizzate da Salvatore Aresco. (Tutti i diritti riservati)");
+})
--- noti.js
+++ noti.js
@@ -1,4 +1,4 @@
-function newNoti(){
+function newNoti(title, snip){
     var noti = document.createElement("div");
 
     noti.classList = "noti";
@@ -9,8 +9,19 @@ function newNoti(){
     noti.appendChild(notiTitle);
     noti.appendChild(notiText);
 
-    notiTitle.innerHTML = "TEST";
-    notiText.innerHTML = "TEST TEST";
+    notiTitle.innerHTML = title;
+    notiText.innerHTML = snip;
+
+    document.body.appendChild(noti);
+    noti.style.display = "initial";
+
+    setTimeout(function(){
+        noti.style.animation = "fadeOut 0.3s ease";
+        setTimeout(function(){
+            noti.style.display = "none";
+            document.body.removeChild(noti);
+        },300)
+    }, 6000);
 
     console.log(noti);
 }
